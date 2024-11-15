573183245a9b51c65e7bd3de816132955003c599
DanteCampos <dcamposfsa@gmail.com>
2024-11-15 21:49:25 +0000

    Add compiled ASN.1 defs

---
+++ src/asn1_defs/generate_python_asn1_defs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Using the Pycrate compiler (installed with pip)
+pycrate_compiler="/home/openran-br/.local/bin/pycrate_asn1compile.py"
+asn1_file="e2sm-kpm-rc.asn"
+python3 $pycrate_compiler -i $asn1_file -o e2sm_kpm_rc.py
